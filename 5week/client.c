7512380ab35656566fcf279c249ff6d521350399
Lyciih <gnehciyil@gmail.com>
2023-04-14 21:36:51 +0800

    connect success

--- 5week/client.c
+++ 5week/client.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+
 int main()
 {
 	int client_fd = 0;
@@ -23,7 +25,8 @@ int main()
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(3000);
+	server_addr.sin_port = htons(80);
+	inet_pton(AF_INET, "59.127.115.95", &server_addr.sin_addr);
 
 	//connect
 	if(connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
