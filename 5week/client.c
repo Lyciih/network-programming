f80683e8490b846dfc404cf6ededf1c34e00180e
Lyciih <gnehciyil@gmail.com>
2023-04-14 21:11:37 +0800

    test

---
+++ 5week/client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+int main()
+{
+	int client_fd = 0;
+	
+
+	//建立 socket
+	if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+	{
+		perror("socket error\n");
+	}
+	else
+	{
+		printf("socket success %d\n", client_fd);
+	}
+
+	//填 server 資料
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(3000);
+
+	//connect
+	if(connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("connect error \n");
+		printf("%d", client_fd);
+	}
+	else
+	{
+		printf("connect success\n");
+	}
+
+
+
+	close(client_fd);
+	return 0;
+}
---
+++ 5week/server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>		//close()
+#include <string.h>		//memset()
+#include <sys/socket.h>
+#include <netinet/in.h>		//sockaddr_in
+
+int main()
+{
+	int listen_fd = 0;
+	int connect_fd = 0;
+
+	//申請用來 listen 的socket
+	if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket error\n");
+		exit(1);
+	}
+	else
+	{
+		printf("create listen socket success\n");
+	}
+
+
+	//用來填 server 地址的結構
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(3000);
+
+	//bind
+	if((bind(listen_fd,(struct sockaddr *)&server_addr, sizeof(server_addr)) < 0))
+	{
+		perror("bind error\n");
+		exit(1);
+	}
+	else
+	{
+		printf("bind success\n");
+	}
+
+
+	//listen
+	if((listen(listen_fd, 5) < 0))
+	{
+		perror("listen error\n");
+		exit(1);
+	}
+	else
+	{
+		printf("listen success\n");
+	}
+
+
+	//accept
+	socklen_t len;
+	struct sockaddr_in client_addr;
+	len = sizeof(client_addr);
+
+	if(connect_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &len) < 0)
+	{
+		perror("accept error\n");
+		exit(1);
+	}
+	else
+	{
+		printf("accept success\n");
+		close(connect_fd);
+	}
+
+	
+	close(listen_fd);
+	return 0;
+
+}
